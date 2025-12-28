479b7a55ef7a0dbbcbe1a79ef5abbb534093f9dc
Matt Jackson <matt@jacksonsix.com>
2025-12-27 21:24:20 -0500

    Setup Tailwind CSS v4 and Shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button/index";
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center gap-4 p-8">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Shadcn <span class="text-primary">Svelte</span>
+	</h1>
+	<p class="leading-7 not-first:mt-6 text-muted-foreground">
+		Tailwind CSS v4 Integration
+	</p>
+	<Button>Click me</Button>
+</div>
